4fba8c3f75fc47b68af0377cf460973b67993119
PremKumar <kpumar135@gmail.com>
2024-08-11 02:51:56 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'kp-us-visa',
+    version = '0.0.0',
+    author = 'KP',
+    author_email= 'kpkumar135@gmail.com',
+    packages = find_packages()
+)
